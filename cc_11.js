c6b9f8887a75aed7ec65a09161decbec9653ac83
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-03-02 19:48:28 -0500

    Task 1 - Created Book Class

---
+++ cc_11.js
@@ -0,0 +1,24 @@
+//Task 1 Creating a Book Class
+class Book {
+    constructor(title, author, isbn, copies) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.copies = copies;
+    }
+    getDetails() {
+        return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies: ${this.copies}`
+    };//book details
+    updateCopies(quantity) {
+        this.copies += quantity;
+    }//modifies available copies when book is borrowed or return
+};
+const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
+
+book1.updateCopies(-1);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
+//Task 2
